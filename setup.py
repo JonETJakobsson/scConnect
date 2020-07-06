25ae4cf84d0fc73c8602ef0ac838d9b584c6d29c
Jon Jakobsson <33955709+JonETJakobsson@users.noreply.github.com>
2020-07-06 12:05:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,20 @@ files = glob.glob("scConnect/**/*.*", recursive=True) # add all files under scCo
 files = [path.replace("\\", "/") for path in files] # Change from windows style to requred style for setuptools
 files = [path.replace("scConnect/", "") for path in files]
 
+with open("README.rts", "r") as fh:
+    description = fh.read()
 
 setup(
    name='scConnect',
    version='1.0.0',
-   summary='scConnect integrate gene expression profiles in scRNA-seq datasets with ligand and receptor interaction information from Guide to pharmacology to construct a graph containing all putative interaction between cell types in the dataset.'
-   description='Connects cell types together based on mathing ligand and receptor pairs.',
+   summary='scConnect integrate gene expression profiles in scRNA-seq datasets with ligand and receptor interaction information from Guide to pharmacology to construct a graph containing all putative interaction between cell types in the dataset.',
+   description=description,
    author='Jon E.T. Jakobsson',
    author_email='jon.jakobsson@neuro.uu.se',
-   Project_URL='GitHub, https://github.com/JonETJakobsson/scConnect',
-   Project_URL='Documentation, https://scconnect.readthedocs.io/en/latest/'
+   Project-URL='GitHub, https://github.com/JonETJakobsson/scConnect',
+   Project-URL='Documentation, https://scconnect.readthedocs.io/en/latest/'
    license='License :: OSI Approved :: MIT License',
-   key_words='scRNA-seq,connectome,ligands,receptors,interactions,sequencing'
+   key-words='scRNA-seq,connectome,ligands,receptors,interactions,sequencing'
    packages=["scConnect"],
    package_data={
       "scConnect": files},
